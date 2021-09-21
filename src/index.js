1ed7f38b0bd3374c113c2f3fe1a5df24c4f724db
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-09-22 00:35:27 +0300

    Add initla todo html , css , and javasctipt

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
+const button = document.querySelector('button');
+class Todo {
+  constructor(description, completed, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
 }
 
-document.body.appendChild(component());
+const todos = [
+  new Todo('Hello world 1', false, 0),
+  new Todo('Hello world 2', false, 1),
+  new Todo('Hello world 5', false, 4),
+  new Todo('Hello world 3', false, 2),
+  new Todo('Hello world 4', false, 3),
+];
+
+function populate() {
+  todos.sort((a, b) => (a.index > b.index ? 1 : -1));
+  todos.forEach((todo) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+    <div class="flex">
+      <div>
+          <input type="checkbox">
+          <span>${todo.description}</span>
+      </div>
+      <span class="material-icons">
+          more_vert
+      </span>
+    </div>
+    <hr>`;
+
+    button.parentElement.insertBefore(li, button);
+  });
+}
+
+window.addEventListener('load', () => {
+  populate();
+});
